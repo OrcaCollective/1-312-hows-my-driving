64f2f2cf1c1e441fa95e34e580da56d3d828c57a
Madison Bowden <bowdenm@spu.edu>
2020-10-27 11:06:53 -0700

    General refactor (#8)
    * Make dataset path declaration agnostic to working directory
    
    * Unify webpages via templating
    
    * Names -> Badges
    
    In the future there will be a tab for names as well
    
    * Move dataset lookup to separate module, use template

--- src/app.py
+++ src/app.py
@@ -1,126 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from csv import DictReader
-from decimal import Decimal
-from pathlib import Path
-from typing import Dict
-
 from flask import Flask, request, render_template
-from sodapy import Socrata
 
-# Support for gomix's 'front-end' and 'back-end' UI.
+import dataset
+
+
 app = Flask(__name__, static_folder="public", template_folder="views")
 
-# Set up the sodapy client
-client = Socrata("data.seattle.gov", None)
-LICENSE_DATASET = "enxu-fgzb"
-SALARY_DATASET = "2khk-5ukd"
-BADGE_DATASET_PATH = Path("data/spd-badges.csv")
 
-
-# Set up dataset
-def _data() -> Dict[str, Dict[str, str]]:
-    print("Loading initial badge data")
-    data = {}
-    with BADGE_DATASET_PATH.open("r") as file:
-        reader = DictReader(file)
-        for row in reader:
-            data[row["Serial"]] = row
-
-    return data
-
-
-BADGE_DATASET = _data()
-
-
+################################################################################
+# Licenses
+################################################################################
 @app.route("/")
-def homepage():
+def license_page():
     """Displays the homepage."""
-    return render_template("index.html")
+    context = {
+        "title": "Seattle Public Vehicle Lookup",
+        "entity_name_long": "license plate",
+        "entity_name_short": "License #",
+        "entity": "license",
+        "data_source": "https://data.seattle.gov/City-Business/Active-Fleet-Complement/enxu-fgzb",
+        "lookup_url": "license-lookup"
+    }
+    return render_template("index.html", **context)
 
 
 @app.route("/license-lookup", methods=["POST"])
 def license_lookup():
-    """Simple API endpoint for dreams.
-    In memory, ephemeral, like real dreams.
-    """
-
-    html = ""
-    # Add a dream to the in-memory database, if given.
-    if "license" in request.args:
-        license = request.args["license"]
-        try:
-            results = client.get(
-                LICENSE_DATASET, limit=1, where=f"license='{license.upper()}'"
-            )
-            if not results:
-                html = "<p><b>No vehicle found for this license</b></p>"
-            else:
-                r = results[0]
-                html = f"""
-<p><b>License:</b> {r['license']}</p>
-<p><b>Make:</b> {r['make']}</p>
-<p><b>Model:</b> {r['model']}</p>
-<p><b>Department:</b> {r['dept']}</p>
-<p><b>Description:</b> {r['descrip']}</p>
-<p><b>Equipment Type:</b> {r['equipment_type']}</p>
-"""
-
-        except Exception as err:
-            print(f"Error: {err}")
-            html = f"<p><b>Error:</b> {err}"
-        print("Final HTML:\n{}".format(html))
-
+    license = request.args.get("entity", "")
+    html = dataset.license_lookup(license)
     # Return the list of remembered dreams.
     return html
 
 
+################################################################################
+# Badges
+################################################################################
 @app.route("/badge")
-def badge():
-    return render_template("badges.html")
+def badge_page():
+    context = {
+        "title": "Seattle Officer Badge Lookup",
+        "entity_name_long": "badge number",
+        "entity_name_short": "Badge #",
+        "entity": "badge",
+        "data_source": "https://data.seattle.gov/City-Business/City-of-Seattle-Wage-Data/2khk-5ukd",
+        "lookup_url": "badge-lookup"
+    }
+    return render_template("index.html", **context)
 
 
 @app.route("/badge-lookup", methods=["POST"])
 def badge_lookup():
-    html = ""
-    if "badge" in request.args:
-        badge = request.args["badge"]
-        try:
-            r = BADGE_DATASET.get(badge)
-            if not r:
-                html = "<p><b>No officer found for this badge number</b></p>"
-            else:
-                first = r["FirstName"]
-                last = r["Surname"]
-                middle = r["MiddleInitMostly"]
-                if middle:
-                    name = f"{first} {middle} {last}"
-                else:
-                    name = f"{first} {last}"
-                html = f"""
-<p><b>Badge #:</b> {r['Serial']}</p>
-<p><b>Name:</b> {name}</p>
-<p><b>Rank:</b> {r['RankRole']}</p>
-"""
-                results = client.get(
-                    SALARY_DATASET,
-                    limit=1,
-                    where=f"department='Police Department' AND last_name='{last}' AND first_name='{first}'",
-                )
-                if results:
-                    s = results[0]
-                    projected = Decimal(s["hourly_rate"]) * 40 * 50
-                    html += f"""
-<p><b>Job Title:</b> {s['job_title']}</p>
-<p><b>Hourly Rate:</b> ${s['hourly_rate']}</p>
-<p><b>Projected salary (w/o overtime):</b> ${projected:,}</p>
-"""
-
-        except Exception as err:
-            print(f"Error: {err}")
-            html = f"<p><b>Error:</b> {err}"
-        print("Final HTML:\n{}".format(html))
-
+    badge = request.args.get("entity")
+    html = dataset.badge_lookup(badge)
     # Return the list of remembered dreams.
     return html
 
---
+++ src/dataset.py
@@ -0,0 +1,86 @@
+from csv import DictReader
+from decimal import Decimal
+from pathlib import Path
+from typing import Dict
+
+from flask import render_template
+from sodapy import Socrata
+
+
+# Set up the sodapy client
+client = Socrata("data.seattle.gov", None)
+LICENSE_DATASET = "enxu-fgzb"
+SALARY_DATASET = "2khk-5ukd"
+BADGE_DATASET_PATH = Path(__file__).parent / "data" / "spd-badges.csv"
+
+
+def _data() -> Dict[str, Dict[str, str]]:
+    """Set up initial dataset"""
+    print("Loading initial badge data")
+    data = {}
+    with BADGE_DATASET_PATH.open("r") as file:
+        reader = DictReader(file)
+        for row in reader:
+            data[row["Serial"]] = row
+
+    return data
+
+
+BADGE_DATASET = _data()
+
+
+def license_lookup(license: str) -> str:
+    try:
+        results = client.get(
+            LICENSE_DATASET, limit=1, where=f"license='{license.upper()}'"
+        )
+        if not results:
+            html = "<p><b>No vehicle found for this license</b></p>"
+        else:
+            r = results[0]
+            html = render_template("license.j2", **r)
+
+    except Exception as err:
+        print(f"Error: {err}")
+        html = f"<p><b>Error:</b> {err}"
+
+    print("Final HTML:\n{}".format(html))
+    return html
+
+
+def badge_lookup(badge: str) -> str:
+    try:
+        r = BADGE_DATASET.get(badge)
+        if not r:
+            html = "<p><b>No officer found for this badge number</b></p>"
+        else:
+            first = r["FirstName"]
+            last = r["Surname"]
+            middle = r["MiddleInitMostly"]
+            if middle:
+                name = f"{first} {middle} {last}"
+            else:
+                name = f"{first} {last}"
+            context = {
+                **r,
+                "name": name
+            }
+
+            results = client.get(
+                SALARY_DATASET,
+                limit=1,
+                where=f"department='Police Department' AND last_name='{last}' AND first_name='{first}'",
+            )
+            if results:
+                s = results[0]
+                projected = Decimal(s['hourly_rate']) * 40 * 50
+                # Format with commas
+                context = {**context, **s, "projected": f"{projected:,}"}
+            html = render_template("badge.j2", **context)
+
+    except Exception as err:
+        print(f"Error: {err}")
+        html = f"<p><b>Error:</b> {err}"
+
+    print("Final HTML:\n{}".format(html))
+    return html
