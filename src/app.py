e7b72c9a8921b111c580ab8ec3d15eec429b153b
Madison Bowden <bowdenm@spu.edu>
2020-10-28 11:44:52 -0700

    Add name to badge lookup (#11)
    * Move salary augmentation out to separate function
    
    * Add unit description to badge output
    
    * Remove entity fields in context now that JS is gone
    
    * Add initial last name lookup
    
    * Make name results unique by name and sort when processing
    
    * Store names by serial for improved uniqueness
    
    Sometimes a middle initial will differ for someone of the same record so it's better to sort these by serial
    
    * Rename badge.j2 to officer.j2

--- src/app.py
+++ src/app.py
@@ -15,7 +15,6 @@ LICENSE_CONTEXT = {
     "title": "Seattle Public Vehicle Lookup",
     "entity_name_long": "license plate",
     "entity_name_short": "License #",
-    "entity": "license",
     "data_source": "https://data.seattle.gov/City-Business/Active-Fleet-Complement/enxu-fgzb",
     "lookup_url": "license-lookup",
 }
@@ -40,7 +39,6 @@ BADGE_CONTEXT = {
     "title": "Seattle Officer Badge Lookup",
     "entity_name_long": "badge number",
     "entity_name_short": "Badge #",
-    "entity": "badge",
     "data_source": "https://data.seattle.gov/City-Business/City-of-Seattle-Wage-Data/2khk-5ukd",
     "lookup_url": "badge-lookup",
 }
@@ -55,6 +53,29 @@ def badge_page():
 def badge_lookup(badge):
     html = dataset.badge_lookup(badge)
     return render_template("index.html", **BADGE_CONTEXT, entity_html=html)
+
+
+################################################################################
+# Names
+################################################################################
+NAME_CONTEXT = {
+    "title": "Seattle Officer Name Lookup",
+    "entity_name_long": "last name",
+    "entity_name_short": "Lastname",
+    "data_source": "https://data.seattle.gov/City-Business/City-of-Seattle-Wage-Data/2khk-5ukd",
+    "lookup_url": "name-lookup",
+}
+
+
+@app.route("/name")
+def name_page():
+    return render_template("index.html", **NAME_CONTEXT)
+
+
+@app.route("/name-lookup/<name>")
+def name_lookup(name):
+    html = dataset.name_lookup(name)
+    return render_template("index.html", **NAME_CONTEXT, entity_html=html)
 
 
 if __name__ == "__main__":
--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 from csv import DictReader
 from decimal import Decimal
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Tuple
 
 from flask import render_template
 from sodapy import Socrata
@@ -12,21 +13,36 @@ client = Socrata("data.seattle.gov", None)
 LICENSE_DATASET = "enxu-fgzb"
 SALARY_DATASET = "2khk-5ukd"
 BADGE_DATASET_PATH = Path(__file__).parent / "data" / "spd-badges.csv"
+BADGE_NAME_DATA_FIELDS = [
+    "Serial",
+    "Surname",
+    "MiddleInitMostly",
+    "FirstName",
+    "RankRole",
+    "UnitDesc",
+]
+_DataDict = Dict[str, Dict[str, str]]
+_DataNameLookup = Dict[str, _DataDict]
 
 
-def _data() -> Dict[str, Dict[str, str]]:
+def _data() -> Tuple[_DataDict, _DataNameLookup]:
     """Set up initial dataset"""
     print("Loading initial badge data")
-    data = {}
+    badges = {}
+    names = defaultdict(dict)
     with BADGE_DATASET_PATH.open("r") as file:
         reader = DictReader(file)
         for row in reader:
-            data[row["Serial"]] = row
+            r = {k: row[k] for k in BADGE_NAME_DATA_FIELDS}
+            badges[row["Serial"]] = r
+            # There may be numerous rows per unique name, so this makes them
+            # easiest to index appropriately/overwrite with the most current value
+            names[row["Surname"]][row["Serial"]] = r
 
-    return data
+    return badges, names
 
 
-BADGE_DATASET = _data()
+BADGE_DATASET, NAME_DATASET = _data()
 
 
 def license_lookup(license: str) -> str:
@@ -48,35 +64,63 @@ def license_lookup(license: str) -> str:
     return html
 
 
+def _augment_with_salary(record: Dict[str, str]) -> Dict[str, str]:
+    first = record["FirstName"]
+    last = record["Surname"]
+    middle = record["MiddleInitMostly"]
+    if middle:
+        name = f"{first} {middle} {last}"
+    else:
+        name = f"{first} {last}"
+    context = {**record, "name": name}
+
+    results = client.get(
+        SALARY_DATASET,
+        limit=1,
+        where=f"department='Police Department' AND last_name='{last}' AND first_name='{first}'",
+    )
+    if results:
+        s = results[0]
+        projected = Decimal(s["hourly_rate"]) * 40 * 50
+        # Format with commas
+        context = {**context, **s, "projected": f"{projected:,}"}
+
+    return context
+
+
 def badge_lookup(badge: str) -> str:
     try:
         r = BADGE_DATASET.get(badge)
         if not r:
             html = "<p><b>No officer found for this badge number</b></p>"
         else:
-            first = r["FirstName"]
-            last = r["Surname"]
-            middle = r["MiddleInitMostly"]
-            if middle:
-                name = f"{first} {middle} {last}"
-            else:
-                name = f"{first} {last}"
-            context = {
-                **r,
-                "name": name
-            }
+            context = _augment_with_salary(r)
+            html = render_template("officer.j2", **context)
 
-            results = client.get(
-                SALARY_DATASET,
-                limit=1,
-                where=f"department='Police Department' AND last_name='{last}' AND first_name='{first}'",
-            )
-            if results:
-                s = results[0]
-                projected = Decimal(s['hourly_rate']) * 40 * 50
-                # Format with commas
-                context = {**context, **s, "projected": f"{projected:,}"}
-            html = render_template("badge.j2", **context)
+    except Exception as err:
+        print(f"Error: {err}")
+        html = f"<p><b>Error:</b> {err}"
+
+    print("Final HTML:\n{}".format(html))
+    return html
+
+
+def _sort_names(record: _DataDict):
+    for r in sorted(record.values(), key=lambda x: x["FirstName"]):
+        yield r
+
+
+def name_lookup(name: str) -> str:
+    try:
+        records = NAME_DATASET.get(name)
+        if not records:
+            html = "<p><b>No officer found for this name</b></p>"
+        else:
+            htmls = []
+            for r in _sort_names(records):
+                context = _augment_with_salary(r)
+                htmls.append(render_template("officer.j2", **context))
+            html = "\n<br/>\n".join(htmls)
 
     except Exception as err:
         print(f"Error: {err}")
