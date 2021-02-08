7b2f5c68ba2ac6423f43779238a30f4d68fcc27c
Madison Swain-Bowden <bowdenm@spu.edu>
2021-02-07 18:50:11 -0800

    Add API types

---
+++ src/api_types.py
@@ -0,0 +1,29 @@
+from typing import TypedDict, List, Dict
+
+
+class SearchRoute(TypedDict):
+    path: str
+    query_params: List[str]
+
+
+class FieldMetadata(TypedDict):
+    FieldName: str
+    Label: str
+
+
+class DatasetMetadata(TypedDict):
+    id: str
+    name: str
+    last_available_roster_date: str
+    fields: List[FieldMetadata]
+    search_routes: Dict[str, SearchRoute]
+
+
+class Record(TypedDict, total=False):
+    first_name: str
+    last_name: str
+
+
+class Entity(TypedDict):
+    entity_name: str
+    query_param: str
