a3028361599bc4482086ad11b3e84281ef1e7a6e
Madison Swain-Bowden <bowdenm@spu.edu>
2021-05-23 17:03:53 -0700

    Prepare for historical data (#49)
    * Cache salary lookups to improve query performance for multiple rosters
    
    * Cast all values to string before evaluating
    
    * Change wording on most recent roster

--- src/extras.py
+++ src/extras.py
@@ -1,27 +1,37 @@
 from decimal import Decimal
-from typing import Dict, Callable
+from functools import lru_cache
+from typing import Dict, Callable, Optional
 
 from flask import render_template
 from soda_api import client, SALARY_DATASET
 from api_types import Record, DEFAULT_DATASET
 
 
-def _augment_with_salary(record: Record) -> str:
-    last = record["last_name"]
-    first = record["first_name"]
+@lru_cache(maxsize=1000)
+def _augment_with_salary_cached(
+    last: Optional[str], first: Optional[str]
+) -> Optional[str]:
+    """Cached augmentation for faster retrieval."""
     results = client.get(
         SALARY_DATASET,
         limit=1,
         where=f'last_name="{last}" AND first_name="{first}"',
     )
-    if results:
-        s = results[0]
-        projected = Decimal(s["hourly_rate"]) * 40 * 50
-        # Format with commas
-        context = {**s, "projected": f"{projected:,}"}
-        return render_template("seattle_extras.j2", **context)
+    if not results:
+        return ""
+    s = results[0]
+    projected = Decimal(s["hourly_rate"]) * 40 * 50
+    # Format with commas
+    context = {**s, "projected": f"{projected:,}"}
+    return render_template("seattle_extras.j2", **context)
+
+
+def augment_with_salary(record: Record) -> Optional[str]:
+    last = record["last_name"]
+    first = record["first_name"]
+    return _augment_with_salary_cached(last, first)
 
 
 EXTRAS_MAPPING: Dict[str, Callable[[Record], str]] = {
-    DEFAULT_DATASET: _augment_with_salary,
+    DEFAULT_DATASET: augment_with_salary,
 }
